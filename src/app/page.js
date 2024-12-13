4dbb58982738802caa2fe2b51ade2e9fb6b84336
Senpai <clevercoder0307@gmail.com>
2024-12-13 19:43:25 +0530

    images changed

--- src/app/page.js
+++ src/app/page.js
@@ -73,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
